d69eb23fe8fc97e9d482a1ec1f5cc3edf48e5ba7
leomourap <105673165+leomourap@users.noreply.github.com>
2022-05-17 16:03:06 -0300

    First Finalized Version
    now, it can be run in Linux terminal with arguments

--- ncrfgffdataextractor1-1.py
+++ ncrfgffdataextractor1-1.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-""" BEFORE THE START, YOU MUST REMOVE THE GFF STANDARD HEADER AND INPUT A NEW ONE:
-
-Cromossomo    Genbank    regiao    pbinicio    pbfim    .    +    .    annotation
-
-(you can adapt if you want, if so, remember to change the columns names in the loop lines of the code)."""
-
 import pandas as pd
+import sys
+
+print("\nUsage: ncrfgffextractor1-1.py NCRFsummaryfile GFFfile output.csv [PREFIX]\n")
+
+print("remember!!! BEFORE THE START, YOU MUST REMOVE THE GFF STANDARD HEADER AND INPUT A NEW ONE:")
+print("Cromossomo    Genbank    regiao    pbinicio    pbfim    .    +    .    annotation(tabdelimited)")
+print("(you can adapt if you want, if so, remember to change the columns names in the loop lines of the code).")
 
 #read NCRF.summarynofilter archive (contains information about the sequence arrays localization);
-df = pd.read_table("ameCgoSat75-21.noise20.summarynofilter")
+try:
+    df = pd.read_table(str(sys.argv[1]))
+except:
+    df = pd.read_table(input("NCRF summary file: "))
 
 #store the information of the NCRF archive into variables to utilize as index later;
 var1 = df.iloc[:,2].tolist() #index of scaffold / chromosome.
@@ -19,10 +23,15 @@ var2 = df.iloc[:,3].tolist() #sequence array beginning base pair.
 
 var3 = df.iloc[:,4].tolist() #sequence array ending base pair.
 
+
 #open annotation.GFF file;
-df2 = pd.read_table("GCA_019721115.1_AMEX_1.1_genomic.GFF") #remember, it must have the new columns names!
+try:
+    df2 = pd.read_table(str(sys.argv[2])) #remember, it must have the new columns names!
+except:
+    df2 = pd.read_table(input("GFF annotation file: "))
 
 df2 = df2.dropna(how="any", axis=0) #treatment for deleting lines with empty values.
+
 
 #extraction of data:
 df5 = pd.DataFrame() #creating the final dataframe (empty by now)
@@ -35,11 +44,13 @@ for i in range(len(var1)):
     #here you can change the additional downstream/upstream base pair portion for extraction.
     df5 = df5.append(df4)
 
-#remove duplicated results - two or more arrays close would generate that.
 df5 = df5.drop_duplicates(subset=None, keep='first', inplace=False, ignore_index=False)
 
 float_col = df5.select_dtypes(include=['float64']) #This will select float columns and store it in a var. for the loop below:
 for col in float_col: #loop to transform the columns "pbinicio" and "pbfim" into integers
     df5[col] = df5[col].astype('int64')
 
-df5.to_csv('GFFextractor1.csv', index=False) #save the final dataframe to an csv.
+try:
+    df5.to_csv(sys.argv[3], index=False) #save the final dataframe to an csv.
+except:
+    df5.to_csv(input("outputfile.csv"), index=False)
